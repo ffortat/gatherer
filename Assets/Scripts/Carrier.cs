2ae28825c2863ae99b09ffcb12d3cf96cab13457
Florent Fortat <florent@fortat.fr>
2022-04-18 13:04:39 +0200

    Implement Carrier functions

--- Assets/Scripts/Carrier.cs
+++ Assets/Scripts/Carrier.cs
@@ -1,7 +1,34 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Carrier : MonoBehaviour
 {
     [SerializeField]
     private Transform storage = null;
+
+    private List<Transform> cubeStore = new List<Transform>();
+
+    public void CarryCube(Transform cube)
+    {
+        cubeStore.Add(cube);
+        cube.parent = storage;
+    }
+
+    public Transform DropCube()
+    {
+        if (HasCube())
+        {
+            Transform cube = cubeStore[0];
+            cubeStore.RemoveAt(0);
+
+            return cube;
+        }
+
+        return null;
+    }
+
+    public bool HasCube()
+    {
+        return cubeStore.Count > 0;
+    }
 }
