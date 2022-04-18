fe7ad522bceaeace8560e0bc6840747d84800356
Florent Fortat <florent@fortat.fr>
2022-04-18 11:12:16 +0200

    Add base carrier

---
+++ Assets/Scripts/Carrier.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carrier : MonoBehaviour
+{
+    [SerializeField]
+    private Transform storage = null;
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,25 +2,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private const string tileTag = "Tile";
+    private const string shedTag = "Shed";
+
     private Mover mover = null;
+    private Carrier carrier = null;
 
     private void Awake()
     {
         mover = GetComponentInChildren<Mover>();
+        carrier = GetComponentInChildren<Carrier>();
     }
 
     private void Update()
     {
         RaycastHit hit;
-        LayerMask tileMask = LayerMask.GetMask("Tile");
+        LayerMask tileMask = LayerMask.GetMask(tileTag, shedTag);
 
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, tileMask))
+        if (Physics.Raycast(GetMouseRay(), out hit, tileMask))
         {
             if (Input.GetMouseButton(0))
             {
-                mover.MoveTo(hit.point);
+                switch (hit.collider.tag)
+                {
+                    case tileTag:
+                        mover.MoveTo(hit.point);
+                        break;
+                    case shedTag:
+                        break;
+                    default:
+                        break;
+                }
             }
         }
+    }
+
+    private static Ray GetMouseRay()
+    {
+        return Camera.main.ScreenPointToRay(Input.mousePosition);
     }
 
     public Mover Mover { get => mover; }
