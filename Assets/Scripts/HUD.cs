9b76a8b7d669d93aa8ce241ed43707ec18b6d29a
Florent Fortat <florent@fortat.fr>
2022-04-18 02:54:14 +0200

    Add HUD to display current speed for debug

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI speedUI = null;
+
+    private PlayerController playerController = null;
+
+    private void Awake()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+    }
+
+    private void Update()
+    {
+        speedUI.text = playerController.Mover.Speed.ToString("F2");
+    }
+}
